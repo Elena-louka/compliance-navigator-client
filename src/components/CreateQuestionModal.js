5bca9ca494897bd771011a4abe93b7e3e7a68595
elena-louka <louka.elen@gmail.com>
2024-03-24 04:39:30 +0200

    small refactoring

--- src/components/CreateQuestionModal.js
+++ src/components/CreateQuestionModal.js
@@ -5,7 +5,7 @@ const CreateQuestionModal = ({ isOpen, onClose, children }) => {
 
   return (
     <div className="fixed inset-0 bg-gray-500 bg-opacity-75 z-50 flex items-center justify-center">
-      <div className="bg-white rounded-lg shadow-xl m-4 sm:max-w-xl sm:w-full sm:m-8 relative"> {/* Add relative here */}
+      <div className="bg-white rounded-lg shadow-xl m-4 sm:max-w-xl sm:w-full sm:m-8 relative">
         {/* Close Button */}
         <button onClick={onClose} className="absolute top-0 right-0 p-2 text-gray-700 hover:text-gray-900">
           <svg className="h-6 w-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
@@ -16,8 +16,6 @@ const CreateQuestionModal = ({ isOpen, onClose, children }) => {
         <div className="p-4">
           {children}
         </div>
-        {/* Moved Create Question Button inside Modal Content for now */}
-        {/* Depending on your design, you might want to place this button differently */}
       </div>
     </div>
   );
--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -4,7 +4,6 @@ function TopBar() {
     return (
         <header className="bg-blue-800 text-white text-xl py-4 px-6 flex justify-between items-center">
             <h1 className="font-bold">ComplianceNavigator</h1>
-            {/* Add navigation items if needed */}
         </header>
     );
 }
